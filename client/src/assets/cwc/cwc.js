b369b45a89059aa5644bca692106b4eac653782a
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-06-11 18:03:32 +0530

    updated the status of cwc members

--- client/src/assets/cwc/cwc.js
+++ client/src/assets/cwc/cwc.js
@@ -3,5 +3,6 @@ import Sneha from './Sneha.jpeg';
 import Ansubha from './Ansubha.jpeg';
 import Srija from './Srija.jpeg';
 import Arkadeep from './Arkadeep.jpeg';
+import Somsuddha from './Somsuddha.jpeg';
 
-export const cwcimage = [Arkadeep, Agnik, Srija, Ansubha, Sneha,"no link"];
+export const cwcimage = [Arkadeep, Agnik, Srija, Ansubha, Sneha, Somsuddha];
