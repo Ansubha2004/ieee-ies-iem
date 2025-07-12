eeb54c74c12dbc77efc938062561b66e70de3d46
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-07-12 18:49:28 +0530

    working on about page

---
+++ client/src/components/Aboutcontent.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Aboutcontent() {
+
+    const scrollTo=(id)=>{
+    const element = document.querySelector(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+    return (
+        <div className="hidden border-solid border-[2px] border-amber-600 h-fit md:flex flex-col min-w-[20%] box-border bg-white sticky md:top-[150px] lg:top-[190px]">
+            <div id="heading" className="bg-amber-600 p-2 text-white font-bold text-[1.3rem]">Content</div>
+            <div className="p-2">
+                <ol className="flex flex-col gap-1">
+                    <li>
+                        <button onClick={() => scrollTo("#About")} className="animatedbutton2 ">About the chapter</button>
+                    </li>
+                    <li>
+                        <button onClick={() => scrollTo("#Mission")} className="animatedbutton2"> Mission </button>
+                    </li>
+                    <li>
+                        <button onClick={() => scrollTo("#Vision")} className="animatedbutton2"> Vision </button>
+                    </li>
+                    <li>
+                        <button onClick={() => scrollTo("#Membership")} className="animatedbutton2"> Membership Details </button>
+                    </li>
+                    <li>
+                        <button onClick={() => scrollTo("#Benifits")} className="animatedbutton2"> Membership Benifits </button>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default Aboutcontent
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Aboutcontent from '../components/Aboutcontent'
 
 function About() {
     return (
-        <div className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center z-[1] box-border  "></div>
+        <div className="relative  min-h-[calc(100vh-7.6rem)] flex px-[5%] py-[20px] sm:py-[40px] text-justify z-[1] box-border gap-5  w-full">
+            <Aboutcontent />
+            <div className="min-w-full md:min-w-[80%] h-fit z-[2] px-7 space-y-5 ">
+                <div id="About" className="w-full">
+                    <p className="heading">About The Chapter</p>
+                    <p className="subheading">Introducing IEM's IEEE IES Students Chapter</p>
+                    <div className="paratext mt-3 space-y-2">
+                        <p>The IEEE Industrial Electronics Society (IES) Student Chapter at the <a href="https://iem.edu.in/" className="linkanimate text-zinc-700">Institute of Engineering & Management (IEM)</a>, Kolkata was officially established on 5th May 2025 under the aegis of the Department of Electrical Engineering. The chapter is recognized by the <a href="https://www.ieee.org/" className="linkanimate text-zinc-700">IEEE</a> with the official Student Branch Chapter (SBC) Code: SBC14301J.
+                        </p>
+                        <p>Serving as a vibrant hub for students interested in industrial electronics, automation, control systems, and intelligent technologies, the chapter is guided by its esteemed advisor, Dr. Sourav Das.
+                        </p>
+                        <p>As part of the globally connected <a href="https://www.ieee-ies.org/" className="linkanimate text-zinc-700">IEEE IES</a> community, this student chapter is committed to:</p>
+                        <ul className="list-disc pl-7">
+                            <li>Hosting technical workshops, seminars, and webinars</li>
+
+                        <li>Facilitating industrial visits and field-based learning</li>
+
+                        <li>Encouraging innovation, research, and technical publications</li>
+
+                        <li>Organizing conferences, competitions, and collaborative projects</li>
+                        </ul>
+                        
+                        <p>Driven by a mission to bridge classroom learning with real-world applications, the chapter empowers students to explore cutting-edge industrial technologies and prepares them for impactful careers in the field.</p>
+                    </div>
+                </div>
+                <div id="Mission" className="w-full ">misssion</div>
+                <div id="Vision" className="w-full ">vision</div>
+                <div id="Membership" className="w-full">membership</div>
+                <div id="Benifits" className="w-full ">benifits</div>
+            </div>
+        </div>
     )
 }
 
