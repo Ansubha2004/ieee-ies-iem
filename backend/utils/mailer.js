19fb295ac1bef05c29a0c6b387e6f7dfb8c5c79b
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-07-20 17:07:32 +0530

    fixed backend

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -29,19 +29,16 @@ const SendMail = async (name, email, message) => {
         text: `Dear ${name},\n\nThank you for reaching out to the IEEE Industrial Electronics Society Student Branch Chapter at the Institute of Engineering & Management (IEEE IES SBC, IEM).\nWe have received your query and appreciate your interest in our chapter.\n\nOur team will get back to you shortly with the necessary information or support. If your query is urgent, please feel free to reach out to us directly at ieeeies.iem@gmail.com .\nWe look forward to connecting with you and hope to see you involved in our upcoming events and initiatives.\n\nWarm regards,\nTean IEEE - IEM\n📧 ieeeies.iem@gmail.com\n🌐 https://ieee-ies-iem-sbc.vercel.app`
     }
 
-    await transporter.sendMail(mail1,(error,info)=>{
-        if(error)
-            console.log("Error in mail 1")
-        else
-        console.log("Message 2 mailed successfully...");
-    })
-
-    await transporter.sendMail(mail2,(error,info)=>{
-        if(error)
-            console.log("Error in mail 2")
-        else
-        console.log("Message 2 mailed successfully...");
-    })
+   try{
+     const info1=await transporter.sendMail(mail1);
+     console.log("Mail 1 sent successfully.....");
+     const info2=await transporter.sendMail(mail2);
+     console.log("Mail 2 sent successfully.....");
+   }
+   catch(error)
+   {
+     console.log("Error sending mails: ",error);
+   }
 
 
 }
