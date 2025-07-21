4919acd7d3ab320c68624fd4d54084911ac8a1fd
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-07-22 00:43:26 +0530

    new home ga;lery

--- client/src/components/Homegallery.jsx
+++ client/src/components/Homegallery.jsx
@@ -5,38 +5,37 @@ import { FiArrowRightCircle } from "react-icons/fi";
 
 function Homegallery() {
 
-    const [index,setIndex]=useState(0);
+  const [index, setIndex] = useState(0);
 
-    const scrollleft=()=>{
-        setIndex((prev)=>{
-            return (prev-1+images.length)%images.length;
-        })
-    }
+  const scrollleft = () => {
+    setIndex((prev) => {
+      return (prev - 1 + images.length) % images.length;
+    })
+  }
 
-    const scrollright=()=>{
-        setIndex((prev)=>{
-            return (prev+1)%images.length;
-        })
-    }
+  const scrollright = () => {
+    setIndex((prev) => {
+      return (prev + 1) % images.length;
+    })
+  }
 
   return (
-    <div className="full flex md:flex-row flex-col justify-center gap-5 md:gap-10 items-center ">
-      <FiArrowLeftCircle onClick={scrollleft} size={70} className="arrowbutton md:block hidden" />
-      <div className="w-full relative border-solid border-[7px] border-amber-700  p-[10px] sm:p-[15px] sm:w-[65%] mt-[30px] bg-yellow-500 aspect-[16/10]  ">
-        <div className="h-full  min-w-full whitespace-nowrap overflow-hidden flex gap-5 bg-transparent">
-          <img
-            id={index}
-            src={images[index]}
-            className="h-full min-w-full "
-            alt=""
-          />
-        </div>
-      </div>
-      <FiArrowRightCircle onClick={scrollright} size={70} className="arrowbutton md:block hidden" />
-      <div className="md:hidden  flex justify-center gap-[5%] w-full">
-        <FiArrowLeftCircle onClick={scrollleft} size={30} className="arrowbutton " />
-        <FiArrowRightCircle onClick={scrollright} size={30} className="arrowbutton " />
-      </div>
+    <div className="w-full flex flex-wrap gap-2 justify-center items-center mt-[30px]">
+      {
+        images.map((image, index) => (
+          <div key={index} className="w-full md:w-[45%] lg:w-[30%] relative border-solid border-[7px] border-amber-700 p-[7px] bg-yellow-500 aspect-[16/10]  ">
+            <div className="h-full  min-w-full whitespace-nowrap overflow-hidden flex gap-5 bg-transparent">
+              <img
+                id={index}
+                src={image}
+                className="h-full min-w-full "
+                alt=""
+              />
+            </div>
+          </div>
+        ))
+      }
+
     </div>
   );
 }
