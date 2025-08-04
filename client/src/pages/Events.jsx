7e7d4ec3c94c668f6f20f4e45aeebe8adb1ac368
Ansubha Dhar <anusbhadhar@Anusbhas-Air.lan>
2025-08-04 21:13:04 +0530

    hello

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Events() {
   return (
-    <div className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center z-[1] box-border  "></div>
+    <div className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center z-[1] box-border  ">hello</div>
   )
 }
 
