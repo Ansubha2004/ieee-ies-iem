9f603fe7945331eab9d87c882ea4b57d6456bc60
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-08-04 22:36:45 +0530

    preparing event pagw

---
+++ client/src/components/EventBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+
+function EventBanner() {
+    return (
+        <div className="relative z-[2] bg-black/20 h-[400px] flex flex-col items-center justify-center gap-10 sm:gap-7 lg:gap-5 w-full ">
+            <p className="font-[500] text-[1.7rem] sm:text-[2rem] text-white/90">IEEE IES IEM - A Events & Programs Hub</p>
+            <p className="w-[90%] text-[1rem] md:text-[0.8rem] text-center text-white/80">Explore a diverse range of technical workshops, innovation-driven competitions, expert talks, and student-led initiatives organized by the IEEE Industrial Electronics Society – IEM Student Chapter. Our events aim to bridge the gap between academia and industry, ignite curiosity, and empower the next generation of engineers and innovators.
+            </p>
+            <Button
+                themecss="py-3 btn2"
+                click={() => {
+                    const section = document.getElementById("view");
+                    if (section) {
+                        section.scrollIntoView({ behavior: "smooth", block: "start" });
+                    }
+                }}
+                Content="Explore the Events"
+            />
+        </div>
+    )
+}
+
+export default EventBanner
--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import orangebg3 from "../assets/designs/orangebg3.png"
+import EventBanner from '../components/EventBanner.jsx';
 
 function Events() {
   return (
-    <div className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center z-[1] box-border  ">hello</div>
+    <div className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center  z-[1] box-border  ">
+      <div className="relative w-full flex justify-center   h-auto">
+        <img src={orangebg3} className="absolute w-full h-[400px] z-[0]" alt="" />
+        <EventBanner/>
+      </div>
+      <div id="view" className="scroll-mt-[108px] lg:scroll-mt-[140px] px-[5%] py-[20px] sm:py-[40px]">
+        <p className="heading text-center">Events & Activity Highlights</p>
+        <p className="subheading text-center">Explore our diverse range of events and activities</p>
+        <br />
+        
+      </div>
+    </div>
   )
 }
 
