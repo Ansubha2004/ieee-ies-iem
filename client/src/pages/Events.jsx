7e226a065c491aeb59b2727bb750a6022f66706c
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-08-05 08:40:19 +0530

    completed first draft of website

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import orangebg3 from "../assets/designs/orangebg3.png"
 import EventBanner from '../components/EventBanner.jsx';
 import Eventdetails from '../components/Eventdetails.jsx';
 import events from "../data/events.json";
 import eventimages from '../utils/eventposter.jsx';
+import { useLocation } from 'react-router-dom';
 
 
 function Events() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }, 100); // slight delay ensures layout is ready
+      }
+    }
+  }, [location]);
+
   return (
-    <div id="eventbanner"  className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center  z-[1]  box-border  ">
+    <div id="eventbanner"  className="relative  min-h-[calc(100vh-7.6rem)] flex flex-col items-center scroll-mt-[100px] lg:scroll-mt-[140px]  z-[1]  box-border  ">
       <div className="relative w-full flex justify-center   h-auto">
         <img src={orangebg3} className="absolute w-full h-[400px] z-[0]" alt="" />
         <EventBanner />
