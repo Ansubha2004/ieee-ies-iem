480cd314f1f16ed2a6c9dc5924e1cd913af4da2d
Ansubha Dhar <ansubhadhar2012@gmail.com>
2026-06-04 11:57:35 +0530

    correction in form

--- admin/src/components/Addmemberform.jsx
+++ admin/src/components/Addmemberform.jsx
@@ -1,75 +1,89 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Button from "./Button.jsx";
 import { IoMdAdd } from "react-icons/io";
 import axios from "axios";
 import { FilesIcon } from "lucide-react";
 
 function Addmemberform() {
-
-  const [formdata,setformdata]=useState({
-    id:"",
-    name:"",
-    role:"",
-    image:"",
-    linkedin:"",
-    email:"",
-    description:""
+  const [formdata, setformdata] = useState({
+    id: "",
+    name: "",
+    role: "",
+    image: "",
+    linkedin: "",
+    email: "",
+    description: "",
   });
 
-  const handlechange=(e)=>{
-    const {name,value,type,files}=e.target;
-    const shallowcopy={...formdata};
-    shallowcopy[name]=type==="file"?files[0]:value;
+  const handlechange = (e) => {
+    const { name, value, type, files } = e.target;
+    const shallowcopy = { ...formdata };
+    shallowcopy[name] = type === "file" ? files[0] : value;
     setformdata(shallowcopy);
-  }
+  };
 
-  const handlesubmit=async (e)=>{
+  const handlesubmit = async (e) => {
     e.preventDefault();
-    const {id,name,role,image,linkedin,email,description}=formdata;
-    if(!id.trim() || !name.trim() || !role.trim() || !image || !linkedin.trim() || !email.trim() || description.trim() )
-    {
-      console.log("Kindly fill the credentials...")
+    const { id, name, role, image, linkedin, email, description } = formdata;
+    if (
+      !id.trim() ||
+      !name.trim() ||
+      !role.trim() ||
+      !image ||
+      !linkedin.trim() ||
+      !email.trim() ||
+      !description.trim()
+    ) {
+      console.log("Kindly fill the credentials...");
     }
-    try
-    {
-        const url="https://ieee-ies-iem.onrender.com";
-        const response=await axios.post(`${url}/cwcapi/addcwc`,formdata,
-          {
-            headers:{
-              "Content-Type":"application/json"
-            }
-          }
-        );
-        const {success,error,data}=response.data;
-        if (success) {
-          console.log("Data posted successfully");
-         
-          //✅ Reset form data here
-          setformdata({
-            name: "",
-            email: "",
-            message: "",
-          });
-        }
-        if (error) {
-        
-          console.log("Error occured while data posting");
-        }
+    try {
+      const newdata = new FormData();
+
+      newdata.append("id", id);
+      newdata.append("name", name);
+      newdata.append("role", role);
+      newdata.append("image", image);
+      newdata.append("linkedin", linkedin);
+      newdata.append("email", email);
+      newdata.append("description", description);
+      const url = "https://ieee-ies-iem.onrender.com";
+      const response = await axios.post(`${url}/cwcapi/addcwc`, newdata);
+      const { success, error, data } = response.data;
+      if (success) {
+        console.log("Data posted successfully");
+
+        //✅ Reset form data here
+        setformdata({
+          name: "",
+          email: "",
+          message: "",
+        });
+      }
+      if (error) {
+        console.log("Error occured while data posting");
+      }
+    } catch (err) {
+      console.log("API Error posting new member .... : ", err);
     }
-    catch(err){
-      console.log("API Error posting new member .... : ",err);
-    }
-  }
+  };
 
   return (
-    <form method="post" encType="multipart/form-data" className="mt-4 w-full">
+    <form
+      method="post"
+      onSubmit={handlesubmit}
+      encType="multipart/form-data"
+      className="mt-4 w-full"
+    >
       <div className="grid sm:grid-cols-2 gap-2 manrope">
         <div className="flex flex-col">
-          <label htmlFor="id" className="text-[0.8rem] font-[600] text-amber-700 mx-1">
+          <label
+            htmlFor="id"
+            className="text-[0.8rem] font-[600] text-amber-700 mx-1"
+          >
             Enter ID
           </label>
           <input
-          required
+            required
             id="id"
             name="id"
             type="number"
@@ -79,16 +93,18 @@ function Addmemberform() {
             className="inputbox"
             onChange={handlechange}
             value={formdata.id}
-
           />
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="name" className="text-[0.8rem] font-[600] text-amber-700 mx-1">
+          <label
+            htmlFor="name"
+            className="text-[0.8rem] font-[600] text-amber-700 mx-1"
+          >
             Full name
           </label>
           <input
-          required
+            required
             id="name"
             name="name"
             type="text"
@@ -96,17 +112,18 @@ function Addmemberform() {
             className="inputbox"
             onChange={handlechange}
             value={formdata.name}
-
-
           />
         </div>
 
         <div className="flex flex-col sm:col-span-1">
-          <label htmlFor="role" className="text-[0.8rem] font-[600] text-amber-700 mx-1">
+          <label
+            htmlFor="role"
+            className="text-[0.8rem] font-[600] text-amber-700 mx-1"
+          >
             Role / position
           </label>
           <input
-          required
+            required
             id="role"
             name="role"
             type="text"
@@ -114,17 +131,18 @@ function Addmemberform() {
             className="inputbox"
             onChange={handlechange}
             value={formdata.role}
-
-
           />
         </div>
 
         <div className="flex flex-col sm:col-span-1">
-          <label htmlFor="image" className="text-[0.8rem] font-[600] text-amber-700 mx-1">
+          <label
+            htmlFor="image"
+            className="text-[0.8rem] font-[600] text-amber-700 mx-1"
+          >
             Profile photo
           </label>
           <input
-          required
+            required
             id="image"
             name="image"
             type="file"
@@ -134,14 +152,15 @@ function Addmemberform() {
           />
         </div>
 
-        
-
         <div className="flex flex-col">
-          <label htmlFor="linkedin" className="text-[0.8rem] font-[600] text-amber-700 mx-1">
+          <label
+            htmlFor="linkedin"
+            className="text-[0.8rem] font-[600] text-amber-700 mx-1"
+          >
             LinkedIn profile URL
           </label>
           <input
-          required
+            required
             id="linkedin"
             name="linkedin"
             type="url"
@@ -149,16 +168,18 @@ function Addmemberform() {
             className="inputbox"
             onChange={handlechange}
             value={formdata.linkedin}
-
           />
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="mail" className="text-[0.8rem] font-[600] text-amber-700 mx-1">
+          <label
+            htmlFor="mail"
+            className="text-[0.8rem] font-[600] text-amber-700 mx-1"
+          >
             Email address
           </label>
           <input
-          required
+            required
             id="mail"
             name="email"
             type="email"
@@ -166,7 +187,6 @@ function Addmemberform() {
             className="inputbox"
             onChange={handlechange}
             value={formdata.email}
-
           />
         </div>
 
@@ -178,7 +198,7 @@ function Addmemberform() {
             Description
           </label>
           <textarea
-          required
+            required
             id="description"
             name="description"
             rows="5"
@@ -186,16 +206,13 @@ function Addmemberform() {
             className="inputbox h-[130px] resize-y"
             onChange={handlechange}
             value={formdata.description}
-
           />
         </div>
-       
       </div>
 
       <div className="flex flex-wrap gap-3 mt-5 mb-3 justify-end">
         <Button
           type="submit"
-          submit={handlesubmit}
           themecss="btn1 flex items-center justify-center gap-2 px-6 py-2 text-[0.9rem]"
           icon={<IoMdAdd />}
           Content="Save member"
