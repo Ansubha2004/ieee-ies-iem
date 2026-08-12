9d12682073281162c9f0405f8cc5f33d7cfd6951
Ansubha Dhar <ansubhadhar2012@gmail.com>
2026-08-12 20:06:26 +0530

    integrating baasic add events and check all events backend

--- backend/controllers/eventcontroller.js
+++ backend/controllers/eventcontroller.js
@@ -1,2 +1,67 @@
 import eventmodel from "../models/eventmodel.js";
 import cloudinary from "../config/cloudinary.js";
+
+export const addevent=async(req,res)=>{
+    try {
+        const {id,link,name,description,venue,eventdate,gallery,details,status}=req.body;
+        if (!name || !id || !link || !description || !venue || !date || !gallery || !details || !status) {
+            return res.json({
+              success: false,
+              message: "Fill the event details",
+            });
+          }
+          if (!req.file) {
+            return res.status(400).json({
+              success: false,
+              message: "Image not uploaded",
+            });
+          }
+          const base64Image = `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`;
+          const result = await cloudinary.uploader.upload(base64Image, {
+            folder: "events_folder",
+          });
+      
+          const createevent = await eventmodel.create({
+            id,
+            link,
+            role,
+            poster: result.secure_url,
+            posterid: result.public_id,
+            name,
+            description,
+            venue,
+            eventdate,
+            gallery,
+            details,
+            status
+          });
+          return res.json({
+            success: true,
+            message: "All event details uploaded successfully",
+            data: createevent,
+          });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error adding event data to db",
+            error: error.message,
+          });
+    }
+}
+
+export const getallevents = async (req, res) => {
+    try {
+      const allevents = await eventmodel.find().sort({ id: 1 });
+      return res.json({
+        success: true,
+        message: "All events data fetched successfully",
+        data: allevents,
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: "Error fetching all event details",
+        error: error.message,
+      });
+    }
+  };
--- backend/routes/eventroutes.js
+++ backend/routes/eventroutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import upload from "../middlewares/uploadimage.js";
+import { addevent, getallevents } from "../controllers/eventcontroller.js";
 
 const router = express.Router();
 
+router.post("/addevent",upload.single("poster"),addevent);
+router.get("/getallevents",getallevents);
 
 
 export default router;
